de67757afd9672e98de274743657ba0140b16d1d
Adam Fabicki <adam.fabicki@gmail.com>
2014-01-27 00:04:52 -0500

    Initial draft of Granita::slurp_csv and Granita::find_if_present. Untested.

--- lib/granita.rb
+++ lib/granita.rb
@@ -1,5 +1,21 @@
-require "granita/version"
+require 'granita/version'
+require 'csv'
 
 module Granita
-  # Your code goes here...
+
+  def slurp_csv(file_path, model_class, &block)
+    CSV.foreach(file_path, headers: true) do |row|
+      data = row.to_hash.symbolize_keys
+
+      yield data if block_given?
+
+      object = model_class.new(data, without_protection: true)
+      object.save(validate: false)
+    end
+  end
+
+  def find_if_present(model_class, where_clause = {})
+    model_class.where(where_clause).first if where_clause.present?
+  end
+
 end
